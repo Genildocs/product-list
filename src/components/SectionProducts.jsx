193ce02a3ce82da635998587f9123abf24e2626e
genildocs <genildocs@gmail.com>
2025-02-24 04:18:58 -0300

    commit

--- src/components/SectionProducts.jsx
+++ src/components/SectionProducts.jsx
@@ -1,11 +1,19 @@
-import data from '../data.json';
 import teste from '../assets/images/image-panna-cotta-thumbnail.jpg';
+import { useEffect, useState } from 'react';
 export default function SectionProducts({ children }) {
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    fetch('data.json')
+      .then((res) => res.json())
+      .then((data) => setProduct(data))
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <section className="p-6 md:p-10 lg:p-28">
       {children}
       <div className=" grid grid-cols-1 md:grid-cols-[repeat(3,minmax(0,13.5rem))] gap-y-5 md:gap-5">
-        {data.map((product, index) => (
+        {product.map((product, index) => (
           <div key={index} className="flex flex-col gap-5">
             <div>
               <img
@@ -15,9 +23,13 @@ export default function SectionProducts({ children }) {
               />
             </div>
             <div>
-              <p>{product.category}</p>
-              <p>{product.name}</p>
-              <p>{product.price}</p>
+              <p className="text-[14px] font-normal text-rose-500">
+                {product.category}
+              </p>
+              <p className="font-semibold text-[1rem]">{product.name}</p>
+              <p className="text-red-600 font-semibold text-[1rem]">
+                ${product.price}
+              </p>
             </div>
           </div>
         ))}
